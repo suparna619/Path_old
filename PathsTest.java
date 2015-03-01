e1e09347f1d79ad4eb98dfe375085aa7d862a8fc
Suparna <suparnad@thoughtworks.com>
2015-03-01 13:34:12 +0530

    Paths.java, PathsTest.java, complieRun.sh are created

---
+++ PathsTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import java.util.*;
+
+
+public class PathsTest {
+	
+	@Test
+	public void test_sampleTest(){
+		assertEquals(1,1);
+	}
+}
