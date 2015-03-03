5d4e59dcade330cd3d5238cbc41f669f7092cf23
Suparna <suparnad@thoughtworks.com>
2015-03-03 10:53:56 +0530

    hasDirectFlight methoed description changes

--- Paths.java
+++ Paths.java
@@ -38,6 +38,44 @@ public class Paths {
 	}
 
 	public boolean hasDirectFlight(String source, String destination){
+		if(source==null || 
+			destination==null ||
+			!isStationPresent(source) ||
+			 !isStationPresent(destination) || 
+			 map.get(source)==null)
+			return false;
 		return map.get(source).contains(destination);
 	}
+
+	public static void main(String[] args) {
+		String sourceStation = args[0];
+		String destinationStation = args[1];
+		Paths newPath = new Paths();
+		if(!newPath.isStationPresent(sourceStation)){
+			System.out.println("No city named '"+sourceStation+"' in database");
+			return;
+		}
+		if(!newPath.isStationPresent(destinationStation)){
+			System.out.println("No city named '"+destinationStation+"' in database");
+			return;
+		}
+		else
+			System.out.println(newPath.hasDirectFlight(sourceStation,destinationStation));
+	}
 }
+
+// public static void main(String[] args) {
+// 		String source = args[0];
+// 		String destination = args[1];
+// 		Paths paths = new Paths();
+// 		if(!paths.isCityPresent(source)){
+// 			System.out.println("No city named '"+source+"' in database");
+// 			return;
+// 		}
+// 		if(!paths.isCityPresent(destination)){
+// 			System.out.println("No city named '"+destination+"' in database");
+// 			return;
+// 		}
+// 		else
+// 			System.out.println(paths.findFlight(source,destination));
+// 	}
