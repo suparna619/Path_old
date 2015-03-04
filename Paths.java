fa0530ae4802040215f44e9dd239218472f41b8c
Suparna <suparnad@thoughtworks.com>
2015-03-04 11:06:28 +0530

    path tested for Bangalore-Tokyo

--- Paths.java
+++ Paths.java
@@ -3,62 +3,85 @@ import java.util.*;
 
 public class Paths {
 	static Map<String, List<String>> map = new HashMap<String, List<String>>();
-
+	List<String> sourceToDestinationPath = new ArrayList<String>();
+	
 	static{
 		List<String> bangalore = new ArrayList<String>();
 		bangalore.add("Singapore");
 		List<String> singapore = new ArrayList<String>();
+		singapore.add("Bangalore");
 		singapore.add("Seoul");
 		singapore.add("Dubai");
 		List<String> seoul = new ArrayList<String>();
+		seoul.add("Singapore");
 		seoul.add("Beijing");
 		List<String> beijing = new ArrayList<String>();
 		beijing.add("Tokyo");
-
-		map.put("Bangalore",bangalore);
-		map.put("Singapore",singapore);
-		map.put("Seoul",seoul);
-		map.put("Beijing",beijing);
+		beijing.add("Seoul");
+		List<String> dubai = new ArrayList<String>();
+		dubai.add("Singapore");
+		List<String> tokyo = new ArrayList<String>();
+		tokyo.add("Beijing");
+		
+		map.put("Bangalore", bangalore);
+		map.put("Singapore", singapore);
+		map.put("Seoul", seoul);
+		map.put("Beijing", beijing);
+		map.put("Dubai", dubai);
+		map.put("Tokyo", tokyo);
 	}
 
 	public boolean isStationPresent(String station){
 		Set<String> sourceStations = map.keySet();
-
 		if(sourceStations.contains(station))
 			return true;
-		else {
-			for(String source : sourceStations) {
-				List<String> destinations = map.get(source);
+		else{
+			for(String source : sourceStations){
+				List<String> destinations = map.get(source) ;
 				if(destinations.contains(station))
 					return true;
 			}
 		}
-
 		return false;
 	}
 
-	public boolean hasDirectFlight(String source, String destination){
-		if(source==null || 
-			destination==null ||
-			!isStationPresent(source) ||
-			 !isStationPresent(destination) || 
-			 map.get(source)==null)
+	public boolean hasWay(String source, String destination){
+		sourceToDestinationPath.add(source);
+		return (findWay(source,destination)) ? true : false;
+	}
+
+	public boolean findWay(String source, String destination){
+		if(map.get(source) == null)
 			return false;
-		return map.get(source).contains(destination);
+		if(map.get(source).contains(destination)){
+			sourceToDestinationPath.add(destination);
+			return true;
+		}
+		if(!map.get(source).contains(destination)){			
+			int size = map.get(source).size();
+			for(int i = 0; i < size; i++){
+				if(!sourceToDestinationPath.contains(map.get(source).get(i))){
+					sourceToDestinationPath.add(map.get(source).get(i));
+					return findWay(map.get(source).get(i),destination);
+				}
+			}
+		}
+		return false;
 	}
 
 	public static void main(String[] args) {
-		String sourceStation = args[0];
+		String sourceStaton = args[0];
 		String destinationStation = args[1];
 		Paths newPath = new Paths();
-		if(!newPath.isStationPresent(sourceStation)){
-			System.out.println("No city named '"+sourceStation+"' in database");
+		if(!newPath.isStationPresent(sourceStaton)){
+			System.out.println("No city named '" + sourceStaton + "' in database");
 			return;
 		}
 		if(!newPath.isStationPresent(destinationStation)){
-			System.out.println("No city named '"+destinationStation+"' in database");
+			System.out.println("No city named '" + destinationStation + "' in database");
 			return;
 		}
-		System.out.println(newPath.hasDirectFlight(sourceStation,destinationStation));
+		else
+			System.out.println(newPath.hasWay(sourceStaton,destinationStation));
 	}
 }
--- PathsTest.java
+++ PathsTest.java
@@ -24,80 +24,111 @@ public class PathsTest {
 	}
 
 	@Test
-	public void hasDirectFlight_returns_true_for_Bangalore_to_Singapore() {
+	public void hasWay_returns_true_for_path_between_Bangalore_to_Singapore() {
 		Paths newPath = new Paths();
-		assertTrue(newPath.hasDirectFlight("Bangalore","Singapore"));
+		assertTrue(newPath.hasWay("Bangalore","Singapore"));
+		assertTrue(newPath.hasWay("Singapore","Bangalore"));
+	}
+
+
+	@Test
+	public void hasWay_returns_true_for_path_between_Singapore_to_Seoul() {
+		Paths newPath = new Paths();
+		assertTrue(newPath.hasWay("Singapore","Seoul"));
+		assertTrue(newPath.hasWay("Seoul","Singapore"));
 	}
 
 	@Test
-	public void hasDirectFlight_returns_false_for_Bangalore_to_Tokyo() {
+	public void hasWay_returns_true_for_path_between_Singapore_to_Dubai() {
 		Paths newPath = new Paths();
-		assertFalse(newPath.hasDirectFlight("Bangalore","Tokyo"));
+		assertTrue(newPath.hasWay("Singapore","Dubai"));
+		assertTrue(newPath.hasWay("Dubai","Singapore"));
 	}
 
 	@Test
-	public void hasDirectFlight_returns_true_for_Singapore_to_Seoul() {
+	public void hasWay_returns_true_for_path_between_Seoul_to_Beijing() {
 		Paths newPath = new Paths();
-		assertTrue(newPath.hasDirectFlight("Singapore","Seoul"));
+		assertTrue(newPath.hasWay("Seoul","Beijing"));
+		assertTrue(newPath.hasWay("Beijing","Seoul"));
 	}
 
 	@Test
-	public void hasDirectFlight_returns_true_for_Singapore_to_Dubai() {
+	public void hasWay_returns_true_for_path_between_Beijing_to_Tokyo() {
 		Paths newPath = new Paths();
-		assertTrue(newPath.hasDirectFlight("Singapore","Dubai"));
+		assertTrue(newPath.hasWay("Beijing","Tokyo"));
+		assertTrue(newPath.hasWay("Tokyo","Beijing"));
 	}
 
 	@Test
-	public void hasDirectFlight_returns_true_for_Seoul_to_Beijing() {
+	public void hasWay_returns_false_for_path_between_Singapur_to_Beijing() {
 		Paths newPath = new Paths();
-		assertTrue(newPath.hasDirectFlight("Seoul","Beijing"));
+		assertFalse(newPath.hasWay("Singapore","Beijing"));
+		assertFalse(newPath.hasWay("Beijing","Singapore"));
+	}
+	
+	@Test
+	public void hasWay_returns_false_for_NULL_to_Tokyo() {
+		Paths newPath = new Paths();
+		assertFalse(newPath.hasWay(null,"Tokyo"));
 	}
 
 	@Test
-	public void hasDirectFlight_returns_true_for_Beijing_to_Tokyo() {
+	public void hasWay_returns_false_for_Singapore_to_NULL() {
 		Paths newPath = new Paths();
-		assertTrue(newPath.hasDirectFlight("Beijing","Tokyo"));
+		assertFalse(newPath.hasWay("Singapore",null));
 	}
 
 	@Test
-	public void hasDirectFlight_returns_false_for_Bangalore_to_Seoul() {
+	public void hasWay_returns_false_for_path_between_null_to_null() {
 		Paths newPath = new Paths();
-		assertFalse(newPath.hasDirectFlight("Bangalore","Seoul"));
+		assertFalse(newPath.hasWay(null,null));
 	}
 
 	@Test
-	public void hasDirectFlight_returns_false_for_Bangalore_to_Dubai() {
-		Paths newPath = new Paths();
-		assertFalse(newPath.hasDirectFlight("Bangalore","Dubai"));
+	public void hasPath_returns_true_if_there_is_flight_between_Bangalore_and_Tokyo (){
+		Paths path = new Paths();
+		assertTrue(path.hasWay("Bangalore","Tokyo"));
 	}
 
 	@Test
-	public void hasDirectFlight_returns_false_for_Singapur_to_Beijing() {
-		Paths newPath = new Paths();
-		assertFalse(newPath.hasDirectFlight("Singapore","Beijing"));
+	public void hasPath_returns_true_if_there_is_flight_between_Tokyo_and_Bangalore (){
+		Paths path = new Paths();
+		assertTrue(path.hasWay("Tokyo", "Bangalore"));
 	}
 
 	@Test
-	public void hasDirectFlight_returns_false_for_Singapore_to_Tokyo() {
+	public void hasWay_returns_true_for_Bangalore_to_Seoul() {
 		Paths newPath = new Paths();
-		assertFalse(newPath.hasDirectFlight("Singapore","Tokyo"));
+		assertTrue(newPath.hasWay("Bangalore","Seoul"));
 	}
 
 	@Test
-	public void hasDirectFlight_returns_false_for_NULL_to_Tokyo() {
+	public void hasWay_returns_true_for_Seoul_to_Bangalore() {
 		Paths newPath = new Paths();
-		assertFalse(newPath.hasDirectFlight(null,"Tokyo"));
+		assertTrue(newPath.hasWay("Seoul","Bangalore"));
 	}
 
 	@Test
-	public void hasDirectFlight_returns_false_for_Singapore_to_NULL() {
+	public void hasWay_returns_true_for_Bangalore_to_Dubai() {
 		Paths newPath = new Paths();
-		assertFalse(newPath.hasDirectFlight("Singapore",null));
+		assertTrue(newPath.hasWay("Bangalore","Dubai"));
 	}
 
 	@Test
-	public void hasDirectFlight_returns_false_for_null_to_null() {
+	public void hasWay_returns_true_for_Dubai_to_Bangalore() {
 		Paths newPath = new Paths();
-		assertFalse(newPath.hasDirectFlight(null,null));
+		assertTrue(newPath.hasWay("Dubai","Bangalore"));
 	}
+
+	@Test
+	public void hasWay_returns_true_for_Singapore_to_Tokyo() {
+		Paths newPath = new Paths();
+		assertTrue(newPath.hasWay("Singapore","Tokyo"));
+	}
+
+	@Test
+	public void hasWay_returns_true_for_Tokyo_to_Singapore() {
+		Paths newPath = new Paths();
+		assertTrue(newPath.hasWay("Tokyo","Singapore"));
+	}	
 }
