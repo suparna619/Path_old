6bb90d10f98fef3eb28a887dbb3a47d67c4d495f
Suparna <suparnad@thoughtworks.com>
2015-03-04 16:53:08 +0530

    printPath return the path between Bangalore to Tokyo

--- PathsTest.java
+++ PathsTest.java
@@ -27,14 +27,23 @@ public class PathsTest {
 	public void hasWay_returns_true_for_path_between_Bangalore_to_Singapore() {
 		Paths newPath = new Paths();
 		assertTrue(newPath.hasWay("Bangalore","Singapore"));
-		assertTrue(newPath.hasWay("Singapore","Bangalore"));
 	}
 
+	@Test
+	public void hasWay_returns_true_for_path_between_Singapore_to_Bangalore() {
+		Paths newPath = new Paths();
+		assertTrue(newPath.hasWay("Singapore","Bangalore"));
+	}
 
 	@Test
 	public void hasWay_returns_true_for_path_between_Singapore_to_Seoul() {
 		Paths newPath = new Paths();
 		assertTrue(newPath.hasWay("Singapore","Seoul"));
+	}
+
+	@Test
+	public void hasWay_returns_true_for_path_between_Seoul_to_Singapore() {
+		Paths newPath = new Paths();
 		assertTrue(newPath.hasWay("Seoul","Singapore"));
 	}
 
@@ -42,6 +51,11 @@ public class PathsTest {
 	public void hasWay_returns_true_for_path_between_Singapore_to_Dubai() {
 		Paths newPath = new Paths();
 		assertTrue(newPath.hasWay("Singapore","Dubai"));
+	}
+
+	@Test
+	public void hasWay_returns_true_for_path_between_Dubai_to_Singapore() {
+		Paths newPath = new Paths();
 		assertTrue(newPath.hasWay("Dubai","Singapore"));
 	}
 
@@ -49,6 +63,11 @@ public class PathsTest {
 	public void hasWay_returns_true_for_path_between_Seoul_to_Beijing() {
 		Paths newPath = new Paths();
 		assertTrue(newPath.hasWay("Seoul","Beijing"));
+	}
+
+	@Test
+	public void hasWay_returns_true_for_path_between_Beijing_to_Seoul() {
+		Paths newPath = new Paths();
 		assertTrue(newPath.hasWay("Beijing","Seoul"));
 	}
 
@@ -56,6 +75,11 @@ public class PathsTest {
 	public void hasWay_returns_true_for_path_between_Beijing_to_Tokyo() {
 		Paths newPath = new Paths();
 		assertTrue(newPath.hasWay("Beijing","Tokyo"));
+	}
+
+	@Test
+	public void hasWay_returns_true_for_path_between_Tokyo_to_Beijing() {
+		Paths newPath = new Paths();
 		assertTrue(newPath.hasWay("Tokyo","Beijing"));
 	}
 
@@ -63,6 +87,11 @@ public class PathsTest {
 	public void hasWay_returns_false_for_path_between_Singapur_to_Beijing() {
 		Paths newPath = new Paths();
 		assertFalse(newPath.hasWay("Singapore","Beijing"));
+	}
+
+	@Test
+	public void hasWay_returns_false_for_path_between_Beijing_to_Singapore() {
+		Paths newPath = new Paths();
 		assertFalse(newPath.hasWay("Beijing","Singapore"));
 	}
 	
@@ -85,15 +114,27 @@ public class PathsTest {
 	}
 
 	@Test
-	public void hasPath_returns_true_if_there_is_flight_between_Bangalore_and_Tokyo (){
+	public void hasWay_returns_true_for_path_between_Bangalore_and_Tokyo() {
 		Paths path = new Paths();
 		assertTrue(path.hasWay("Bangalore","Tokyo"));
 	}
 
 	@Test
-	public void hasPath_returns_true_if_there_is_flight_between_Tokyo_and_Bangalore (){
+	public void printPath_returns_path_for_Bangalore_to_Tokyo() {
+		Paths newPath = new Paths();
+		assertEquals(newPath.printPath("Bangalore","Tokyo"),"Bangalore -> Singapore -> Seoul -> Beijing -> Tokyo");
+	}
+
+	@Test
+	public void hasWay_returns_true_for_path_between_Tokyo_and_Bangalore() {
 		Paths path = new Paths();
 		assertTrue(path.hasWay("Tokyo", "Bangalore"));
+	}
+
+	@Test
+	public void printPath_returns_path_for_Tokyo_to_Bangalore() {
+		Paths newPath = new Paths();
+		assertEquals(newPath.printPath("Tokyo", "Bangalore"),"Tokyo -> Beijing -> Seoul -> Singapore -> Bangalore");
 	}
 
 	@Test
